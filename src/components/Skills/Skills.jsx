73745e0b9a05ca30f0d07f011bfaaf0ff9f51fba
kunalambarte17 <ambartekunal17@gmail.com>
2024-12-28 23:57:55 +0530

    New Component was Added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,158 +1,174 @@
-import React from 'react'
-import "./Skills.css"
-import boot from "../../assets/Skill_Logos/bootstrap-icon.svg"
-import css from "../../assets/Skill_Logos/css-icon.svg"
-import express from "../../assets/Skill_Logos/express-js-icon.svg"
-import git from "../../assets/Skill_Logos/git-icon.svg"
-import github from "../../assets/Skill_Logos/github-icon.svg"
-import html from "../../assets/Skill_Logos/html-icon.svg"
-import js from "../../assets/Skill_Logos/js-icon.svg"
-import mongo from "../../assets/Skill_Logos/mongodb-icon.svg"
-import mysql from "../../assets/Skill_Logos/mysql-icon.svg"
-import node from "../../assets/Skill_Logos/node-js-icon.svg"
-import npm from "../../assets/Skill_Logos/npm-icon.svg"
-import post from "../../assets/Skill_Logos/postgresql-icon.svg"
-import react from "../../assets/Skill_Logos/react-js-icon.svg"
-import socket from "../../assets/Skill_Logos/socketicon.svg"
-import tailwind from "../../assets/Skill_Logos/tailwind-css-icon.svg"
-import vscode from "../../assets/Skill_Logos/vscode-icon.svg"
+import './Skills.css'
+import boot from '../../assets/skills/boot.png'
+import css from '../../assets/skills/css.png'
+import express from '../../assets/skills/express.png'
+import figma from '../../assets/skills/figma.png'
+import git from '../../assets/skills/git.png'
+import github from '../../assets/skills/github.svg'
+import html from '../../assets/skills/html.png'
+import js from '../../assets/skills/js.png'
+import materialUi from '../../assets/skills/materialUi.png'
+import mongo from '../../assets/skills/mongo.png'
+import nextjs1 from '../../assets/skills/next.svg'
+import node from '../../assets/skills/node.svg'
+import npm from '../../assets/skills/npm.svg'
+import nvim from '../../assets/skills/nvim.svg'
+import react from '../../assets/skills/react.png'
+import sql from '../../assets/skills/sql.png'
+import vite from '../../assets/skills/vite.png'
+import vscode from '../../assets/skills/vscode.png'
+import tailwind from '../../assets/skills/tailwind.png'
 
-
-function Skills() {
-  // return (
-  //   <div className='skill'>
-  //     <h2 className='mb-3'>My Skills</h2>
-  //     <div className='container'>
-  //       <h4>Frontend</h4>
-  //       <div className='container skill-contain1'>
-  //         <button type="button" className="btn btn-secondary"><img src={html} height={50} /></button>
-  //         <button type="button" className="btn btn-secondary"><img src={css}  height={50}/></button>
-  //         <button type="button" className="btn btn-secondary"><img src={boot} height={50} /></button>
-  //         <button type="button" className="btn btn-secondary"><img src={react} height={50}/></button>
-  //         <button type="button" className="btn btn-secondary"><img src={tail} height={50} /></button>
-  //       </div>
-  //     </div>
-  //     <div className='container'>
-  //       <h4>Backend</h4>
-  //       <div className='container skill-contain1'>
-  //         <button type="button" className="btn btn-secondary"><img src={node} height={50} /></button>
-  //         <button type="button" className="btn btn-secondary"><img src={express}  height={50}/></button>
-  //         <button type="button" className="btn btn-secondary"><img src={java} height={50} /></button>
-  //       </div>
-  //     </div>
-  //     <div className='container'>
-  //       <h4>Development Tools</h4>
-  //       <div className='container skill-contain1'>
-  //         <button type="button" className="btn btn-secondary"><img src={node} height={50} /></button>
-  //         <button type="button" className="btn btn-secondary"><img src={express}  height={50}/></button>
-  //         <button type="button" className="btn btn-secondary"><img src={java} height={50} /></button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
-  return(
-    <div className='container'>
-      <h2 className='skill-title'>My Skills</h2>
-      <div className="box-container">
-        <div className='box'>
-          <div className="mini-box" id='html'>
-            <img src={html} className='s-logo' />
-            <h2 className='l-title'>Html</h2>
-          </div>
+const Skills = () => {
+  return (
+    <div id='skills' className='skills'>
+      <div className="skill-heading">
+        <h1>Skills</h1>
+      </div>
+      <div className="skill-container">
+        <div className="skill-tech-name">
+          <h3 className='skill-tech-heading'>Frontend</h3>
+          <ul className="skill-list">
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#007acc26]">
+                <img src={react} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>React</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#f0db4f26]">
+                <img src={js} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Javascript</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#dd4b2526]">
+                <img src={html} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>HTML</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#264de426]">
+                <img src={css} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>CSS</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#44a8b326]">
+                <img src={tailwind} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Tailwind</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#6d10f426]">
+                <img src={boot} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Bootstrap</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#7ebae226]">
+                <img src={materialUi} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Material UI</span>
+            </li>
+          </ul>
         </div>
-        <div className='box'>
-          <div className="mini-box" id='css'>
-            <img src={css} className='s-logo' />
-            <h2 className='l-title'>Css</h2>
-          </div>
+        <div className="skill-tech-name mt-[3rem]">
+          <h3 className='skill-tech-heading'>Backend</h3>
+          <ul className="skill-list">
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#6fa66026]">
+                <img src={node} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Node.js</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#6fa66026]">
+                <img src={express} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Express</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#6fa66026]">
+                <img src={mongo} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>MongoDb</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#3bbdb314]">
+                <img src={sql} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>MySQL</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#ffffff14]">
+                <img src={nextjs1} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Next.js</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#6e97fe26]">
+                <img src={vite} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Vite</span>
+            </li>
+            {/* <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#007acc26]">
+                <img src={materialUi} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Material UI</span>
+            </li> */}
+          </ul>
         </div>
-        <div className='box'>
-          <div className="mini-box" id='js'>
-            <img src={js} className='s-logo' />
-            <h2 className='l-title'>JavaScript</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='react'>
-            <img src={react} className='s-logo' />
-            <h2 className='l-title'>React</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='boot'>
-            <img src={boot} className='s-logo' />
-            <h2 className='l-title'>Bootstrap</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='tail'>
-            <img src={tailwind} className='s-logo' />
-            <h2 className='l-title'>Tailwind</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='node'>
-            <img src={node} className='s-logo' />
-            <h2 className='l-title'>Nodejs</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='express'>
-            <img src={express} className='s-logo' />
-            <h2 className='l-title'>Expressjs</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='socket'>
-            <img src={socket} className='s-logo' />
-            <h2 className='l-title'>Socket.io</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='npm'>
-            <img src={npm} className='s-logo' />
-            <h2 className='l-title'>Npm</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='mysql'>
-            <img src={mysql} className='s-logo' />
-            <h2 className='l-title'>MySql</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='mongo'>
-            <img src={mongo} className='m-logo' />
-            <h2 className='m-title'>MongoDB</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='post'>
-            <img src={post} className='s-logo' />
-            <h2 className='l-title'>Postgre</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='git'>
-            <img src={git} className='s-logo' />
-            <h2 className='l-title'>Git</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='github'>
-            <img src={github} className='s-logo' />
-            <h2 className='l-title'>Github</h2>
-          </div>
-        </div>
-        <div className='box'>
-          <div className="mini-box" id='vscode'>
-            <img src={vscode} className='s-logo' />
-            <h2 className='l-title'>Vscode</h2>
-          </div>
+        <div className="skill-tech-name mt-[3rem]">
+          <h3 className='skill-tech-heading'>Tools</h3>
+          <ul className="skill-list">
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#33abe326]">
+                <img src={vscode} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>VScode</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#55e53926]">
+                <img src={nvim} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Nvim</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#e671711f]">
+                <img src={git} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Git</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#ffffff14] ">
+                <img src={github} alt="" className='skill-img ' />
+              </div>
+              <span className='skill-name'>Github</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#cc000026]">
+                <img src={npm} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>npm</span>
+            </li>
+            <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#ffffff14]">
+                <img src={figma} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Figma</span>
+            </li>
+            {/* <li className='skill-list-li'>
+              <div className="skill-img-div bg-[#007acc26]">
+                <img src={materialUi} alt="" className='skill-img' />
+              </div>
+              <span className='skill-name'>Material UI</span>
+            </li> */}
+          </ul>
         </div>
       </div>
     </div>
-
   )
 }
 
