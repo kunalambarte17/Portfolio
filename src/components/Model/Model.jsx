78610d42cbe25c33631fbca550fc156f36725bde
kunalambarte17 <ambartekunal17@gmail.com>
2025-01-28 20:06:45 +0530

    Resume was Edited

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Model.css'
-import resume from '../../assets/Resume.png'
+import resume from '../../assets/resume.jpg'
 
 function Model() {
   return (
     <>
       <div className='resume container'>
-        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/10J6sxAYW00MHtX0j--yTdigQy3DZPO1k/view?usp=drive_link' target='_main'>
+        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1JmfhjKkCLPXajH9iKKATvDSh4Eo-lf1r/view?usp=sharing' target='_main'>
         <i className="fa-solid fa-download"></i> Resume
         </a>
         <div className='resume-img'>
