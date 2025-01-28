391012b91396a7e97a2826f79c7fa7362da15bd2
kunalambarte17 <ambartekunal17@gmail.com>
2025-01-28 22:35:40 +0530

    Resume Edited

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -12,6 +12,9 @@ function Model() {
         <div className='resume-img'>
           <img src={resume} alt="" />
         </div>
+        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1hxFkBAR9r-qV_TKP5hyFSqH6NrmOQz0b/view?usp=sharing' target='_main'>
+        <i className="fa-solid fa-download"></i> Resume
+        </a>
       </div>
     </>
   )
