a97d0f3020e13fd71c971b4fb817267e72ac2325
kunalambarte17 <ambartekunal17@gmail.com>
2025-06-18 11:09:26 +0530

    Resume Edited

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -6,13 +6,13 @@ function Model() {
   return (
     <>
       <div className='resume container'>
-        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1hxFkBAR9r-qV_TKP5hyFSqH6NrmOQz0b/view?usp=sharing' target='_main'>
+        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1_j2z2_9XCARpYl9aytQrTLSpFjBOSdQx/view?usp=drivesdk' target='_main'>
         <i className="fa-solid fa-download"></i> Resume
         </a>
         <div className='resume-img'>
           <img src={resume} alt="" />
         </div>
-        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1hxFkBAR9r-qV_TKP5hyFSqH6NrmOQz0b/view?usp=sharing' target='_main'>
+        <a className="btn btn-primary res-b" href='https://drive.google.com/file/d/1_j2z2_9XCARpYl9aytQrTLSpFjBOSdQx/view?usp=drivesdk' target='_main'>
         <i className="fa-solid fa-download"></i> Resume
         </a>
       </div>
