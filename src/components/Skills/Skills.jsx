001df0a5fdec9dae7b142e6a8f663cc4b31b3572
kunalambarte17 <ambartekunal17@gmail.com>
2025-11-21 13:56:45 +0530

    Skills Updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import './Skills.css'
 import boot from '../../assets/skills/boot.png'
 import css from '../../assets/skills/css.png'
@@ -26,159 +26,84 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 const Skills = () => {
-  useEffect(()=> {
-    AOS.init({duration: 2000, once: false , disable: 'mobile, tablet, phone'});
-  }, [])
+  useEffect(() => {
+    AOS.init({ duration: 2000, once: false, disable: 'mobile, tablet, phone' });
+  }, []);
+
+  // Rows of skills (7 items each for smooth duplication)
+  const row1 = [
+    { img: react, name: "React", cls: "react" },
+    { img: js, name: "Javascript", cls: "js" },
+    { img: html, name: "HTML", cls: "html" },
+    { img: css, name: "CSS", cls: "css" },
+    { img: tailwind, name: "Tailwind", cls: "tail" },
+    { img: nextjs1, name: "Next.js", cls: "next" },
+    { img: git, name: "Git", cls: "git" },
+  ];
+
+  const row2 = [
+    { img: node, name: "Node.js", cls: "node" },
+    { img: express, name: "Express", cls: "express" },
+    { img: mongo, name: "MongoDB", cls: "mongo" },
+    { img: sql, name: "MySQL", cls: "sql" },
+    { img: postgre, name: "PostgreSQL", cls: "tail" },
+    { img: socket, name: "Socket.io", cls: "next" },
+    { img: vite, name: "Vite", cls: "vite" },
+  ];
+
+  const row3 = [
+    { img: vscode, name: "VScode", cls: "vs" },
+    { img: nvim, name: "Nvim", cls: "nvim" },
+    { img: github, name: "Github", cls: "hub" },
+    { img: npm, name: "npm", cls: "npm" },
+    { img: materialUi, name: "Material UI", cls: "material" },
+    { img: figma, name: "Figma", cls: "figma" },
+    { img: boot, name: "Bootstrap", cls: "boot" },
+  ];
+
+  // Renders a band. direction: 'left' | 'right'. duration optional (seconds).
+  const renderBand = (skills, direction = 'left', duration = 18) => {
+    const className = direction === 'right' ? 'scroll-right' : 'scroll-left'
+    // pass duration as inline style variable so we can easily tweak per-row if desired.
+    const style = { ['--scroll-duration']: `${duration}s` }
+    return (
+      <div className="infinite-scroll" style={{ margin: '1.25rem 0' }}>
+        <ul className={`skill-list ${className}`} style={style}>
+          {skills.map((s, i) => (
+            <li className="skill-list-li" key={i}>
+              <div className={`skill-img-div ${s.cls}`}>
+                <img src={s.img} alt={s.name} className="skill-img" />
+              </div>
+              <span className="skill-name">{s.name}</span>
+            </li>
+          ))}
+          {/* duplicate for seamless loop */}
+          {skills.map((s, i) => (
+            <li className="skill-list-li" key={'dup-' + i}>
+              <div className={`skill-img-div ${s.cls}`}>
+                <img src={s.img} alt={s.name} className="skill-img" />
+              </div>
+              <span className="skill-name">{s.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   return (
-    <div id='skills' className='skills' data-aos="fade-zoom-in">
+    <div id="skills" className="skills" data-aos="fade-zoom-in">
       <div className="skill-heading">
         <h1>Skills</h1>
       </div>
-      <div className="skill-container">
-        <div className="skill-tech-namez">
-          <h3 className='skill-tech-heading'>Frontend</h3>
-          <ul className="skill-list">
-            <li className='skill-list-li'>
-              <div className="skill-img-div react bg-[#007acc26]">
-                <img src={react} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>React</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div js bg-[#f0db4f26]">
-                <img src={js} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Javascript</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div html bg-[#dd4b2526]">
-                <img src={html} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>HTML</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div css bg-[#264de426]">
-                <img src={css} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>CSS</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div tail bg-[#44a8b326]">
-                <img src={tailwind} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Tailwind</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div boot bg-[#6d10f426]">
-                <img src={boot} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Bootstrap</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div material bg-[#7ebae226]">
-                <img src={materialUi} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Material UI</span>
-            </li>
-          </ul>
-        </div>
-        <div className="skill-tech-name mt-[3rem]">
-          <h3 className='skill-tech-heading'>Backend</h3>
-          <ul className="skill-list">
-            <li className='skill-list-li'>
-              <div className="skill-img-div node bg-[#6fa66026]">
-                <img src={node} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Node.js</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div express bg-[#6fa66026]">
-                <img src={express} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Express</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div mongo bg-[#6fa66026]">
-                <img src={mongo} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>MongoDb</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div sql bg-[#3bbdb314]">
-                <img src={sql} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>MySQL</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div next bg-[#ffffff14]">
-                <img src={socket} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Socket.io</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div tail bg-[#007acc26]">
-                <img src={postgre} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Postgre Sql</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div next bg-[#ffffff14]">
-                <img src={nextjs1} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Next.js</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div vite bg-[#6e97fe26]">
-                <img src={vite} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Vite</span>
-            </li>
-          </ul>
-        </div>
-        <div className="skill-tech-name mt-[3rem]">
-          <h3 className='skill-tech-heading'>Tools</h3>
-          <ul className="skill-list">
-            <li className='skill-list-li'>
-              <div className="skill-img-div vs bg-[#33abe326]">
-                <img src={vscode} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>VScode</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div nvim bg-[#55e53926]">
-                <img src={nvim} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Nvim</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div git bg-[#e671711f]">
-                <img src={git} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Git</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div hub bg-[#ffffff14] ">
-                <img src={github} alt="" className='skill-img ' />
-              </div>
-              <span className='skill-name'>Github</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div npm bg-[#cc000026]">
-                <img src={npm} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>npm</span>
-            </li>
-            <li className='skill-list-li'>
-              <div className="skill-img-div figma bg-[#ffffff14]">
-                <img src={figma} alt="" className='skill-img' />
-              </div>
-              <span className='skill-name'>Figma</span>
-            </li>
-          </ul>
-        </div>
+
+      <div className="skill-container no-titles">
+        {renderBand(row1, 'left', 18)}
+        {renderBand(row2, 'right', 20)} {/* middle row slightly slower */}
+        {renderBand(row3, 'left', 16)}  {/* bottom row slightly faster */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
